0e3e0fd52c27a546ae6947c63c9152164bbeeff8
Laura <laura.robertson64@gmail.com>
2019-10-01 09:54:12 -0500

    fetching data for pages

---
+++ pages/p/[id].js
@@ -0,0 +1,13 @@
+import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
+
+export default function Post() {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <h1>{router.query.id}</h1>
+      <p>This is the blog post content.</p>
+    </Layout>
+  );
+}
